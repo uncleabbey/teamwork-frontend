5efb641239f3713e99c43501a437f18c219383fd
Kayode Gabriel <kayodegabrielA@gmail.com>
2019-11-22 12:07:50 +0100

    Updates

---
+++ precache-manifest.9eca8fc09728112df47165f6732e3ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8522e3a185a2bddcdf5b71e3f155fdf",
+    "url": "uncleabbey.github.io/teamwork-frontend/index.html"
+  },
+  {
+    "revision": "4e589d0e063aa4c771fe",
+    "url": "uncleabbey.github.io/teamwork-frontend/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "c1a82246e2d353c0361b",
+    "url": "uncleabbey.github.io/teamwork-frontend/static/css/main.a5540d75.chunk.css"
+  },
+  {
+    "revision": "4e589d0e063aa4c771fe",
+    "url": "uncleabbey.github.io/teamwork-frontend/static/js/2.bf2ebe16.chunk.js"
+  },
+  {
+    "revision": "c1a82246e2d353c0361b",
+    "url": "uncleabbey.github.io/teamwork-frontend/static/js/main.ffff4179.chunk.js"
+  },
+  {
+    "revision": "8132f41e2d70717b99d2",
+    "url": "uncleabbey.github.io/teamwork-frontend/static/js/runtime-main.f3fbd74c.js"
+  }
+]);
